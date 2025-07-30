cdffed40388977ecca1a546aadb7a7ce853b9a6b
kanchaniitr <kanchan.besuit@gmail.com>
2025-07-30 12:48:21 +0530

    minor refactor

--- src/main/java/com/example/Calendar/CalendarController.java
+++ src/main/java/com/example/Calendar/CalendarController.java
@@ -2,7 +2,6 @@ package com.example.Calendar;
 
 import com.example.Calendar.dto.CountriesDTO;
 import com.example.Calendar.dto.HolidaysDTO;
-import com.example.Calendar.externalApi.HolidayWithName;
 import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.ObjectUtils;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
@@ -34,28 +32,12 @@ public class CalendarController {
         List<HolidaysDTO> holidays = new ArrayList<>();
         if (ObjectUtils.isEmpty(countryCode) || ObjectUtils.isEmpty(startDate) || ObjectUtils.isEmpty(endDate))
             return holidays;
-        holidays = getHolidaysList(countryCode, startDate, endDate);
+        holidays = calendarService.getHolidaysList(countryCode, startDate, endDate);
         return holidays;
     }
 
     @GetMapping("/countries")
     public List<CountriesDTO> getAllCountries() {
-        return calendarService.getAllCountries().stream()
-                .map(dto -> new CountriesDTO(dto.getName(), dto.getCode()))
-                .collect(Collectors.toList());
-    }
-
-    public List<HolidaysDTO> getHolidaysList(String countryCode, String sDate, String eDate) {
-        int year = Integer.parseInt(sDate.substring(0, 4));
-        int sMonth = Integer.parseInt(sDate.substring(4, 6));
-        int eMonth = Integer.parseInt(eDate.substring(4, 6));
-        List<HolidaysDTO> toReturn = new ArrayList<>();
-        List<HolidayWithName> holidays = calendarService.getHolidaysList(countryCode, year);
-        for (HolidayWithName hDay : holidays) {
-            int hMonth = hDay.getDate().getDatetime().getMonth();
-            if (hMonth >= sMonth && hMonth <= eMonth)
-                toReturn.add(new HolidaysDTO(hDay.getDate().toString(), hDay.getName()));
-        }
-        return toReturn;
+        return calendarService.getAllCountries();
     }
 }
--- src/main/java/com/example/Calendar/CalendarService.java
+++ src/main/java/com/example/Calendar/CalendarService.java
@@ -1,14 +1,20 @@
 package com.example.Calendar;
 
-import com.example.Calendar.externalApi.*;
+import com.example.Calendar.dto.CountriesDTO;
+import com.example.Calendar.dto.HolidaysDTO;
+import com.example.Calendar.externalApi.CountriesResponse;
+import com.example.Calendar.externalApi.CountryNameWithCode;
+import com.example.Calendar.externalApi.HolidayResponse;
+import com.example.Calendar.externalApi.HolidayWithName;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Month;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CalendarService {
@@ -21,18 +27,39 @@ public class CalendarService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public List<CountryNameWithCode> getAllCountries() {
+    private List<CountryNameWithCode> fetchCountriesList() {
         CountriesResponse countryResponse = restTemplate.getForObject(countriesURI, CountriesResponse.class);
-        if(!ObjectUtils.isEmpty(countryResponse)){
+        if (!ObjectUtils.isEmpty(countryResponse)) {
             return countryResponse.getResponse().getCountries();
         }
         return Collections.emptyList();
     }
 
-    public List<HolidayWithName> getHolidaysList(String countryCode, int year) {
+    private List<HolidayWithName> fetchHolidayList(String countryCode, int year) {
         String url = String.format("%s&country=%s&year=%d", holidaysURI, countryCode, year);
         HolidayResponse holidays = restTemplate.getForObject(url, HolidayResponse.class);
 
         return holidays.getResponse().getHolidays();
+    }
+
+    public List<CountriesDTO> getAllCountries() {
+        List<CountryNameWithCode> countriesList = fetchCountriesList();
+        return countriesList.stream()
+                .map(dto -> new CountriesDTO(dto.getName(), dto.getCode()))
+                .collect(Collectors.toList());
+    }
+
+    public List<HolidaysDTO> getHolidaysList(String countryCode, String sDate, String eDate) {
+        int year = Integer.parseInt(sDate.substring(0, 4));
+        int sMonth = Integer.parseInt(sDate.substring(4, 6));
+        int eMonth = Integer.parseInt(eDate.substring(4, 6));
+        List<HolidaysDTO> toReturn = new ArrayList<>();
+        List<HolidayWithName> holidays = fetchHolidayList(countryCode, year);
+        for (HolidayWithName hDay : holidays) {
+            int hMonth = hDay.getDate().getDatetime().getMonth();
+            if (hMonth >= sMonth && hMonth <= eMonth)
+                toReturn.add(new HolidaysDTO(hDay.getDate().toString(), hDay.getName()));
+        }
+        return toReturn;
     }
 }
